d65aed6daf34a0daacd44567d8d7e886e15f13d4
Jeremy Likness <jeremy@jeremylikness.com>
2018-10-05 08:50:47 -0700

    001: Initial state

---
+++ README.md
@@ -0,0 +1,13 @@
+# TypeScript from JavaScript
+
+This is a project you can walk through commit-by-commit to see the transformation of JavaScript code to TypeScript.
+
+[@JeremyLikness](https://twitter.com/JeremyLikness)
+
+## Intro
+
+Currently, there is a small JavaScript "app" that you can run with the command:
+
+`node index.js`
+
+There are some defects and if you browse to JavaScript in your IDE, you may or may not get appropriate hints about what's wrong.
---
+++ index.js
@@ -0,0 +1,47 @@
+function delay(fn) {
+    setTimeout(fn, 0);
+}
+
+delay(function() {
+    console.log("\n\nDEBUG INFO:");
+});
+
+function Contact(name, age, contactType, phoneNumber) {
+    this.name = name;
+    this.age = age;
+    this.contactType = contactType;
+    this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.print = function () {
+    console.log(this.name);
+    console.log(this.age);
+    if (this.contactType === "mobile") {
+        console.log("Cell phone:");
+    }
+    else {
+        console.log("Landline:");
+    }
+    console.log(this.contactNumber);
+}
+
+function ContactList(contacts) {
+    this.contacts = contacts;
+}
+
+ContactList.prototype.print = function () {
+    delay(function () { 
+        console.log("About to print ...");
+    });
+    for (var idx = 0; idx < this.contacts.length; idx += 1) {
+        delay(function () {
+            console.log("Printing contact " + idx);
+        });
+        this.contacts[idx].print();
+    }
+}
+
+const me = new Contact("Jeremy", 44.1, "Mobile", "555-1212");
+const myWife = new Contact("Doreen", 30, "Home", "404-123-4567");
+const rolodex = new ContactList(me, myWife);
+rolodex.print();
