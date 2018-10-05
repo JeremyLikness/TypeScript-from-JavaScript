08f09e3c71693a45b8e8998d2005a8ae6fb7be58
Jeremy Likness <jeremy@jeremylikness.com>
2018-10-05 16:45:54 -0700

    002: Refactored to use TypeScript

--- README.md
+++ README.md
@@ -6,8 +6,33 @@ This is a project you can walk through commit-by-commit to see the transformatio
 
 ## Intro
 
+`git checkout d65aed6`
+
 Currently, there is a small JavaScript "app" that you can run with the command:
 
 `node index.js`
 
 There are some defects and if you browse to JavaScript in your IDE, you may or may not get appropriate hints about what's wrong.
+
+## Transform to TypeScript
+
+JavaScript is valid TypeScript (with a few exceptions) so we'll start by setting up TypeScript.
+
+`npm install --save-dev typescript`
+
+Add two build scripts to the `package.json` file for compiling and compiling on a watch:
+
+```javascript
+"scripts": {
+    "build": "tsc",
+    "build:w": "tsc -w"
+}
+```
+
+To create a configuration file that TypeScript uses to compile source to JavaScript, run:
+
+`./node_modules/typescript/bin/tsc --init`
+
+Finally, rename `index.js` to `index.ts`.
+
+> Ouch! There are a lot of errors. We'll address these in the next commit.
--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
-"use strict";
 function delay(fn) {
     setTimeout(fn, 0);
 }
-delay(function () {
+
+delay(function() {
     console.log("\n\nDEBUG INFO:");
 });
+
 function Contact(name, age, contactType, phoneNumber) {
     this.name = name;
     this.age = age;
     this.contactType = contactType;
     this.phoneNumber = phoneNumber;
 }
+
 Contact.prototype.print = function () {
     console.log(this.name);
     console.log(this.age);
@@ -21,12 +23,14 @@ Contact.prototype.print = function () {
         console.log("Landline:");
     }
     console.log(this.contactNumber);
-};
+}
+
 function ContactList(contacts) {
     this.contacts = contacts;
 }
+
 ContactList.prototype.print = function () {
-    delay(function () {
+    delay(function () { 
         console.log("About to print ...");
     });
     for (var idx = 0; idx < this.contacts.length; idx += 1) {
@@ -35,8 +39,9 @@ ContactList.prototype.print = function () {
         });
         this.contacts[idx].print();
     }
-};
-var me = new Contact("Jeremy", 44.1, "Mobile", "555-1212");
-var myWife = new Contact("Doreen", 30, "Home", "404-123-4567");
-var rolodex = new ContactList(me, myWife);
+}
+
+const me = new Contact("Jeremy", 44.1, "Mobile", "555-1212");
+const myWife = new Contact("Doreen", 30, "Home", "404-123-4567");
+const rolodex = new ContactList(me, myWife);
 rolodex.print();
